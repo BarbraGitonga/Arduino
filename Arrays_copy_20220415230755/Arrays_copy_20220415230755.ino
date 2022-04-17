3a6ac089a82e490290e243736046a5d93c0e74df
BarbraGitonga <99233324+BarbraGitonga@users.noreply.github.com>
2022-04-18 00:31:03 +0300

    Update Arrays_copy_20220415230755.ino

--- Arrays_copy_20220415230755/Arrays_copy_20220415230755.ino
+++ Arrays_copy_20220415230755/Arrays_copy_20220415230755.ino
@@ -1,4 +1,3 @@
-int sum = 0;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
@@ -14,7 +13,7 @@ for(int i=0; i<5; i++){// the initial is numbered in index
 for(int j=0; j<5; j++){
    Serial.println(mine[j]+5);
 }
-
+int sum = 0;
 for(int k=0; k<5; k++){
   sum = sum + mine[k];  
   Serial.println(sum);
